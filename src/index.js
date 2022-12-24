1742630ba4940dba00a7fdebc7fed9b3e7d6bc2e
Stefan-RR <stefan.ruchlewicz@outlook.com>
2022-12-24 14:34:36 -0700

    Changed nav button styling

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ function dropDown() {
     document.getElementById('dropDown').classList.toggle('show');
 }
 window.onclick = function(ev){
-    if (ev.target.className !== 'navButton'){
+    if (ev.target.className !== 'button-box'){
         document.getElementById('dropDown').classList.remove('show');
     }
 }
 
 function preventScroll() {
-    document.getElementById('main').classList.toggle('noScroll');
+    document.getElementById('main').classList.toggle('no-scroll');
 }
 window.onclick = function(ev){
-    if (ev.target.className !== 'navButton'){
-        document.getElementById('main').classList.remove('noScroll');
+    if (ev.target.className !== 'button-box'){
+        document.getElementById('main').classList.remove('no-scroll');
     }
 }
