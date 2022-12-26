f9bddbeb5fc578c75417959495e253b3ef8b33db
Stefan-RR <stefan.ruchlewicz@outlook.com>
2022-12-26 15:11:15 -0700

    Combined onclick events to properly hide nav menu when page is clicked

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 function dropDown() {
     document.getElementById('dropDown').classList.toggle('show');
 }
-window.onclick = function(ev){
-    if (ev.target.className !== 'button-box'){
-        document.getElementById('dropDown').classList.remove('show');
-    }
-}
-
 function preventScroll() {
     document.getElementById('main').classList.toggle('no-scroll');
 }
 window.onclick = function(ev){
     if (ev.target.className !== 'button-box'){
         document.getElementById('main').classList.remove('no-scroll');
+        document.getElementById('dropDown').classList.remove('show');
     }
 }
