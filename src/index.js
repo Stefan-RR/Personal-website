b534c682561caddd69dbd613677c2fae325c29dd
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-01-25 23:45:25 -0700

    Added animation on load

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-function dropDown() {
-    document.getElementById('dropDown').classList.toggle('show');
+window.onload = function() {  
+    setTimeout(function() {
+        document.getElementById('top').classList.toggle('top-animation')
+        document.getElementById('bottom').classList.toggle('bottom-animation')
+        document.getElementById('right').classList.toggle('right-animation')
+        document.getElementById('left').classList.toggle('left-animation')
+    }, 900)
 }
-function preventScroll() {
-    document.getElementById('main').classList.toggle('no-scroll');
-}
-window.onclick = function(ev){
-    if (ev.target.className !== 'button-box'){
-        document.getElementById('main').classList.remove('no-scroll');
-        document.getElementById('dropDown').classList.remove('show');
-    }
-}
-window.onload = function() {
-    document.getElementById('top').classList.toggle('top-animation')
-    document.getElementById('bottom').classList.toggle('bottom-animation')
-    document.getElementById('right').classList.toggle('right-animation')
-    document.getElementById('left').classList.toggle('left-animation')
-}
+
