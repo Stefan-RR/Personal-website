9ca1833585aee548e74b750ffd1cb8d24d2fa735
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-01-26 21:06:26 -0700

    Updated font and styling

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ window.onload = function() {
         document.getElementById('bottom').classList.toggle('bottom-animation')
         document.getElementById('right').classList.toggle('right-animation')
         document.getElementById('left').classList.toggle('left-animation')
-    }, 1000)
+    }, 800)
     setTimeout(function() {
         document.getElementById('splash-screen').classList.toggle('hidden')
     }, 2000)
