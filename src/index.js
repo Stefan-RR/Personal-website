6d81ef28db945a9c301f5b6ff4e297a2660fbdfd
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-01-29 16:20:34 -0700

    Added contact form animation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 window.onload = function() {  
     setTimeout(function() {
-        document.getElementById('top').classList.toggle('top-animation')
-        document.getElementById('bottom').classList.toggle('bottom-animation')
-        document.getElementById('right').classList.toggle('right-animation')
-        document.getElementById('left').classList.toggle('left-animation')
+        document.getElementById('top').classList.toggle('top-animation');
+        document.getElementById('bottom').classList.toggle('bottom-animation');
+        document.getElementById('right').classList.toggle('right-animation');
+        document.getElementById('left').classList.toggle('left-animation');
     }, 800)
     setTimeout(function() {
         document.getElementById('splash-screen').classList.toggle('hidden')
     }, 2000)
 }
 
+function contactForm() {
+    document.getElementById('description').classList.toggle('qq');
+    document.getElementById('projects').classList.toggle('qq');
+    document.getElementById('contact').classList.toggle('show');
+    document.getElementById('top').classList.toggle('top-animation');
+    document.getElementById('bottom').classList.toggle('bottom-animation');
+    document.getElementById('right').classList.toggle('right-animation');
+    document.getElementById('left').classList.toggle('left-animation');
+}
